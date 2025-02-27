1c44fca2fd4b14325eaa9a8d5e66d240e5aca85a
Maja Greiner <greinerm.wwi23@student.dhbw-heidenheim.de>
2025-02-27 14:23:10 +0100

    footer passt erstmal

--- routenplanung-aux-neo4j-a1/src/components/footer.tsx
+++ routenplanung-aux-neo4j-a1/src/components/footer.tsx
@@ -3,28 +3,23 @@ import { Mail, Phone, MapPin, Instagram } from "lucide-react";
 export default function Footer() {
   return (
     <footer className="border-t-2 border-blue-500 text-sm">
-
-      <div className="max-w-[1200px] mx-auto grid grid-cols-2 md:grid-cols-4 gap-2 px-2 py-2 items-center justify-items-center">
+      <div className="max-w-[1200px] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 px-2 py-2 justify-items-start">
         
-        {/* E-Mail */}
         <div className="flex items-center gap-1">
           <Mail className="h-4 w-4 text-blue-500" />
           <span>kundencenter(at)avv-augsburg.de</span>
         </div>
-
-        {/* Ort */}
+        
         <div className="flex items-center gap-1">
           <MapPin className="h-4 w-4 text-blue-500" />
           <span>Halderstraße 29, 86150 Augsburg</span>
         </div>
-
-        {/* Telefon */}
+        
         <div className="flex items-center gap-1">
           <Phone className="h-4 w-4 text-blue-500" />
           <span>0821/450 446 16</span>
         </div>
-
-        {/* Instagram */}
+        
         <div className="flex items-center gap-1">
           <Instagram className="h-4 w-4 text-blue-500" />
           <span>@avv_augsburg</span>
